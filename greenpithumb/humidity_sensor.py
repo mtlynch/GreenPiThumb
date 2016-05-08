ecd48384a210cb16f287a5709ec5bc6f68902c83
Jeet Shetty <Jeet Shetty>
2016-05-08 15:28:23 -0400

    update HumiditySensor

--- greenpithumb/humidity_sensor.py
+++ greenpithumb/humidity_sensor.py
@@ -3,25 +3,32 @@ import dht11_exceptions
 class HumiditySensor(object):
 	"""Wrapper for humidity sensor."""
 
-	def __init__(self, dht11_result):
+	def __init__(self, dht11):
 		"""Creates a new HumiditySensor wrapper.
 
 		Args:
-			dht11_result: Result of a reading from a DHT11 humidity and
-			temperature sensor.   
+			dht11: Instance of DHT11 sensor reader class. 
 		"""
-		self._dht11_result = dht11_result
+		self._dht11 = dht11
 
 	def get_humidity_level(self):
 		"""Returns humidity level."""
 
-		if self._dht11_result.error_code() == 1:
-			raise dht11_exceptions.MissingDataError()
-		elif self._dht11_result.error_code() == 2:
-			raise dht11_exceptions.IncorrectCRCError()
-		elif self._dht11_result.error_code() == 0:
-			humidity_level = self._dht11_result.humidity()
-		else:
-			raise ValueError("DHT11 error code out of range")
+		error_code = self._dht11.read().error_code
+
+		# TODO(JeetShetty): Replace error codes with constants from dht11 
+		# module
+		
+		if error_code != 0:
+			if error_code == 1:
+				raise dht11_exceptions.MissingDataError(
+					"DHT11 sensor reported missing data")
+			elif error_code == 2:
+				raise dht11_exceptions.IncorrectCRCError()
+			else:
+				raise ValueError(
+					"DHT11 error code out of range: %i" % error_code)
+
+		humidity_level = self._dht11.read().humidity
 
 		return humidity_level
--- tests/test_humidity_sensor.py
+++ tests/test_humidity_sensor.py
@@ -9,15 +9,15 @@ from greenpithumb import dht11_exceptions
 class HumiditySensorTest(unittest.TestCase):
 
 	def setUp(self):
-		self.mock_dht11_result = mock.Mock()
+		self.mock_dht11 = mock.Mock()
 		self.humidity_sensor = humidity_sensor.HumiditySensor(
-			self.mock_dht11_result)
+			self.mock_dht11)
 
 	def test_humidity_50(self):
 		"""Value given should be value returned."""
 
-		self.mock_dht11_result.error_code.return_value = 0
-		self.mock_dht11_result.humidity.return_value = 50.0
+		self.mock_dht11.read().error_code = 0
+		self.mock_dht11.read().humidity = 50.0
 		humidity_level = self.humidity_sensor.get_humidity_level()
 		self.assertEqual(humidity_level, 50.0)
 
@@ -25,20 +25,20 @@ class HumiditySensorTest(unittest.TestCase):
 		"""Error code of 1 should raise an exception."""
 
 		with self.assertRaises(dht11_exceptions.MissingDataError):
-			self.mock_dht11_result.error_code.return_value = 1
+			self.mock_dht11.read().error_code = 1
 			self.humidity_sensor.get_humidity_level()
 
 	def test_incorrect_crc_error(self):
 		"""Error code of 2 should raise an exception."""
 
 		with self.assertRaises(dht11_exceptions.IncorrectCRCError):
-			self.mock_dht11_result.error_code.return_value = 2
+			self.mock_dht11.read().error_code = 2
 			self.humidity_sensor.get_humidity_level()
 
 	def test_error_code_out_of_range(self):
 		"""Error code other than 0, 1, or 2 should raise an exception."""
 
 		with self.assertRaises(ValueError):
-			self.mock_dht11_result.error_code.return_value = 3
+			self.mock_dht11.read().error_code = 3
 			self.humidity_sensor.get_humidity_level()
 
