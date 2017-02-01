60ef33bba2d46460da1817bc0728949ee479fc6c
Michael Lynch <mtlynch@google.com>
2017-01-31 21:04:27 -0500

    Update

--- greenpithumb/greenpithumb.py
+++ greenpithumb/greenpithumb.py
@@ -70,6 +70,7 @@ def read_wiring_config(config_filename):
 def main(args):
     with contextlib.closing(db_store.open_or_create_db(
             args.data_file)) as db_connection:
+        # TODO(mtlynch): Do something with database here.
         pass
     sensor_harness = SensorHarness(read_wiring_config(args.config_file))
     sensor_harness.print_readings_header()
