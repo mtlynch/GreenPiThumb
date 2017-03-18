6b787b4d626fc762f7b49e2222b84ee049c99daf
Michael Lynch <mtlynch@google.com>
2017-03-18 11:53:06 -0400

    Adding diagnostic logging
    Adding logging to all of the sensors and some of the other components to make
    it easier to see what's going on while GreenPiThumb runs.

--- greenpithumb/poller.py
+++ greenpithumb/poller.py
@@ -1,4 +1,7 @@
+import logging
 import threading
+
+logger = logging.getLogger(__name__)
 
 
 class SensorPollerBase(object):
@@ -18,9 +21,11 @@ class SensorPollerBase(object):
 
     def _poll(self):
         """Polls at a fixed interval until caller calls close()."""
+        logger.info('polling starting for %s', self.__class__.__name__)
         while not self._closed.is_set():
             self._poll_once()
             self._local_clock.wait(self._poll_interval)
+        logger.info('polling terminating for %s', self.__class__.__name__)
 
     def start_polling_async(self):
         """Starts a new thread to begin polling."""
